a14fd73dc5c945ea10e2299829c72dc61fed70ab
victor <vibanez18@gmail.com>
2023-07-08 09:59:14 +0200

    add dependecies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.8.21"
     id("com.android.library")
 }
 
@@ -25,15 +26,33 @@ kotlin {
         }
     }
 
+    val ktorVersion = "2.3.2"
+
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                // Kotlin coroutines
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+
+                // Ktor
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
     }
